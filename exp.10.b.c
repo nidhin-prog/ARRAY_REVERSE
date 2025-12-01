beae8ed6864fd9d0a5ed871921c7d4a4aed6e1a3
nidhinandepalli <nidhi.nn307@gmail.com>
2025-12-01 22:33:46 +0530

    Add files via upload

---
+++ exp.10.b.c
@@ -0,0 +1,14 @@
+*/ NAME:Nidhi Nandepalli
+  UIN:251P092
+  DIV:D */
+#include <stdio.h>
+
+int main() {
+    int array[] = {10, 20, 30, 40, 50};
+    int n = sizeof(array) / sizeof(array[0]);
+    int *p = array;
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%d ", *(p + i));
+    }
+    return 0;
+}
